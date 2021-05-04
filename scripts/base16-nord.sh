c9db17d61d7973ad2e457c2f3ef27f3dfaedc05c
Abram Perdanaputra Situmorang <abram.perdanaputra@gojek.com>
2021-05-04 11:39:17 +0700

    Fix nord

--- scripts/base16-nord.sh
+++ scripts/base16-nord.sh
@@ -3,14 +3,15 @@
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
 # Nord scheme by arcticicestudio
 
+# New
 color00="2E/34/40" # Base 00 - Black
-color01="88/C0/D0" # Base 08 - Red
-color02="BF/61/6A" # Base 0B - Green
-color03="5E/81/AC" # Base 0A - Yellow
-color04="EB/CB/8B" # Base 0D - Blue
-color05="A3/BE/8C" # Base 0E - Magenta
-color06="D0/87/70" # Base 0C - Cyan
-color07="E5/E9/F0" # Base 05 - White
+color01="bf/61/6a" # Base 08 - Red
+color02="a3/be/8c" # Base 0B - Green
+color03="eb/cb/8b" # Base 0A - Yellow
+color04="81/a1/c1" # Base 0D - Blue
+color05="b4/8e/ad" # Base 0E - Magenta
+color06="88/c0/d0" # Base 0C - Cyan
+color07="e5/E9/F0" # Base 05 - White
 color08="4C/56/6A" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
@@ -19,8 +20,8 @@ color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
 color15="8F/BC/BB" # Base 07 - Bright White
-color16="81/A1/C1" # Base 09
-color17="B4/8E/AD" # Base 0F
+color16="d0/87/70" # Base 09
+color17="5e/81/ac" # Base 0F
 color18="3B/42/52" # Base 01
 color19="43/4C/5E" # Base 02
 color20="D8/DE/E9" # Base 04
